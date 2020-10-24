a36ea3e6ea50964ef82b80ee14a11ff613a5122a
Poorvi Parikh <poorviparikh7@gmail.com>
2020-10-24 11:07:32 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/mainops/MainTeleOp.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.teamcode.mainops;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.teamcode.libraries.TeleLib;
+
+
+/*
+ * Title: MainTeleOp
+ * Date Created: 10/14/2018
+ * Date Modified: 2/27/2019
+ * Author: Poorvi, Sachin
+ * Type: Main
+ * Description: This is the main teleop program we will use
+ */
+
+@TeleOp(group = "Main")
+public class MainTeleOp extends LinearOpMode {
+    private TeleLib teleLib;
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        initialize();
+
+        while (opModeIsActive()) {
+            // Gamepad 1
+            teleLib.processDrive();
+            teleLib.processIntakeStone();
+            teleLib.processStopIntake();
+            teleLib.processOutakeStone();
+            teleLib.processFoundation();
+            teleLib.processTapeMeasure();
+//            teleLib.processAutonomousArm();
+
+
+            // Gamepad 2
+            teleLib.processMoveArm();
+            teleLib.processGrabStone();
+            teleLib.processScoreStone();
+            teleLib.processExtendArm();
+            teleLib.processRetractArm();
+            teleLib.processDropCapstone();
+            idle();
+        }
+    }
+
+    private void initialize() {
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        teleLib = new TeleLib(this);
+
+        teleLib.autonomousArmUp();
+        teleLib.autonomousArmGrab();
+        sleep(2000);
+        teleLib.restServoStopper();
+        teleLib.holdCapstone();
+
+        telemetry.addData("Status", "Ready");
+        telemetry.update();
+        waitForStart();
+
+        telemetry.addData("Status", "Running");
+    }
+}
